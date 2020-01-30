3e879d0813bcea385b67f67aecc11dcdb7ee5a7a
Lori Boyd <lori.p.boyd@gmail.com>
2020-01-29 22:53:19 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  def find_artist
+    Artist.all.find(params[:id])
+  end
+
   get "/" do
+    @artists = Artist.all
     erb :welcome
+  end
+
+  get "/artists" do
+    @artists = Artist.all
+    erb :index
+  end
+
+  get "/artists/new" do
+    erb :new
+  end
+
+  get "/artists/:id" do
+    @artist = find_artist
+    erb :show
+  end
+
+  get "/artists/:id/edit" do
+    @artist = find_artist
+    erb :edit
+  end
+
+  get "/artists/:id/delete" do
+    @artist = find_artist
+    erb :delete
+  end
+
+  post "/artists" do
+    p params
+    @artist = Artist.create(params)
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  patch "/artists/:id" do
+    params.delete("_method")
+    @artist = find_artist
+    @artist.update(params)
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  delete "/artists/:id" do
+    @artist = find_artist
+    @artist.destroy
+    redirect to "/artists"
   end
 
 end
